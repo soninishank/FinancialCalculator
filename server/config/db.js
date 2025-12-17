42343bb9dcaa54fa681b997586728e415df8e9f9
nishank.soni <soninishank8@gmail.com>
2025-12-17 13:53:31 +0530

    Updated .env details

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = 'postgresql://neondb_owner:npg_as3VJZkdre9B@ep-polished-hill-a1o9tkl8-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString,
